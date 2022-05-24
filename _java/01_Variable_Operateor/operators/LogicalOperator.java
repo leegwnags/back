45bb409535683acf123a2849db7d99241a0e50d6
leegwnags <102591801+leegwnags@users.noreply.github.com>
2022-05-24 22:01:45 +0900

    learn java Variable_Operator

---
+++ _java/01_Variable_Operateor/operators/LogicalOperator.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class LogicalOperator {
+
+	public static void main(String[] args) {
+		System.out.println("논리연산자 => 리턴타입? boolean");
+		System.out.println("&&, ||, !");
+		
+		// &&, || 연산의 방식을 고려하기
+		System.out.println(false && true); // false
+		System.out.println(true || false); // true
+		System.out.println(!false); //true
+
+	}
+
+}
