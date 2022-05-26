5ac54bbc7659a077afeac38955f5e4fddecd089f
leegwnags <102591801+leegwnags@users.noreply.github.com>
2022-05-26 21:34:31 +0900

    learn java Condition_Loop

---
+++ _java/02_Condition_Loop/quiz/Quiz2.java
@@ -0,0 +1,15 @@
+package quiz;
+
+import java.util.Scanner;
+
+public class Quiz2 {
+
+	public static void main(String[] args) {
+		// Q2. 음수나 0을 입력할 때 까지 숫자를 계속 입력받고
+		// 음수나 0이 입력되면 지금까지 입력된 수의 총합과 평균을 구하세요
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("정수를 입력하세요 > ");
+
+	}
+}
