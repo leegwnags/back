0f38c5071479664b8a61d5fc2bc1cbbb91ca9b44
leegwnags <102591801+leegwnags@users.noreply.github.com>
2022-06-09 21:28:25 +0900

    learn java package_built_in

---
+++ _java/06_Package/built_in/util/StringTokennizerEx.java
@@ -0,0 +1,23 @@
+package built_in.util;
+
+import java.util.StringTokenizer;
+
+public class StringTokennizerEx {
+
+	public static void main(String[] args) {
+		String str = "100,200,300,400";
+		StringTokenizer st = new StringTokenizer(str, ",");
+		
+		while (st.hasMoreTokens()) {
+			System.out.println(st.nextToken());
+		}
+		
+		System.out.println("----------------------------");
+		String exp = "x=100*(100+10)/3";
+		StringTokenizer st2 = new StringTokenizer(exp, "x=*+/()", false);
+		while (st2.hasMoreTokens()) {
+			System.out.println(st2.nextToken());
+		}
+	}
+
+}
