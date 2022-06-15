a519148756cd47999b34b4213c5c78a906789dd2
leegwnags <102591801+leegwnags@users.noreply.github.com>
2022-06-15 21:39:53 +0900

    learn java inheritance

---
+++ _java/07_Inheritance/practice/case1/Ggobugi.java
@@ -0,0 +1,42 @@
+package practice.case1;
+
+public class Ggobugi extends Character {
+
+	public Ggobugi() {
+		hp = 200; // 체력 초기화
+		mp = 100; // 마나 초기화
+	}
+
+	@Override
+	public void eat() {
+		mp += 20;
+	}
+
+	@Override
+	public void sleep() {
+		mp += 30;
+	}
+
+	@Override
+	public void play() {
+		mp -= 30;
+		hp += 15;
+	}
+
+	@Override
+	public boolean train() {
+		mp -= 20;
+		hp += 20;
+		levelUp();
+		return checkMp();
+	}
+
+	@Override
+	public void levelUp() {
+		if (hp >= 80) {
+			hp -= 80;
+			level++;
+		}
+	}
+
+}
---
+++ _java/07_Inheritance/practice/case1/Isangheacci.java
@@ -0,0 +1,42 @@
+package practice.case1;
+
+public class Isangheacci extends Character {
+
+	public Isangheacci() {
+		hp = 150; // 체력 초기화
+		mp = 100; // 마나 초기화
+	}
+
+	@Override
+	public void eat() { 
+		mp += 20;
+	}
+
+	@Override
+	public void sleep() {
+		mp += 40;
+	}
+
+	@Override
+	public void play() {
+		mp -= 30;
+		hp += 15;
+	}
+
+	@Override
+	public boolean train() {
+		mp -= 15;
+		hp += 15;
+		levelUp();
+		return checkMp();
+	}
+
+	@Override
+	public void levelUp() {
+		if(hp >= 60) {
+			hp -= 60;
+			level++;
+		}
+	}
+
+}
