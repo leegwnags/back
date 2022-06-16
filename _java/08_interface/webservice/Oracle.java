cbc8c6f8b644398a0ad58ecca147bc0a93f16d93
leegwnags <102591801+leegwnags@users.noreply.github.com>
2022-06-16 22:10:45 +0900

    learn java interface

---
+++ _java/08_interface/webservice/Oracle.java
@@ -0,0 +1,27 @@
+package webservice;
+
+public class Oracle implements DataBase {
+
+	@Override
+	public boolean insert(String id) {
+			System.out.println("ORACLE > 회원 정보를 입력" 
+					+ (id.length() > 2 ? "성공" : "실패"));
+			return id.length() > 2 ? true : false;
+	}
+
+	@Override
+	public void select() {
+		System.out.println("Oracle 데이터베이스에서 회원 정보를 조회하였습니다");
+	}
+
+	@Override
+	public void update() {
+		System.out.println("Oracle 데이터베이스에서 회원 정보를 수정하였습니다");
+	}
+
+	@Override
+	public void delete() {
+		System.out.println("Oracle 데이터베이스에서 회원 정보를 삭제하였습니다");
+	}
+
+}
