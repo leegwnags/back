2ca463cd980dfb107500c03acc115499dd101903
leegwnags <102591801+leegwnags@users.noreply.github.com>
2022-06-23 19:32:14 +0900

    learn java Collection_Framework

---
+++ _java/10_Collection_Framework/generic/FruitBox.java
@@ -0,0 +1,13 @@
+package generic;
+
+public class FruitBox {
+
+	public static void main(String[] args) {
+		Box<Fruit> fruitBox = new Box<>();
+		Box<Apple> appleBox = new Box<>();
+		Box<Orange> orangeBox = new Box<>();
+//		Box<Toy> toyBox = new Box<>(); 타입 제한
+		
+	}
+
+}
