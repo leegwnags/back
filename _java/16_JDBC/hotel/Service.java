5d9be07a70d33fec98ad63696f128d950e853ec6
leegwnags <102591801+leegwnags@users.noreply.github.com>
2022-07-13 22:20:46 +0900

    learn java JDBC hotel

---
+++ _java/16_JDBC/hotel/Service.java
@@ -0,0 +1,30 @@
+package hotel;
+
+import java.util.List;
+
+public interface Service {
+
+	void initHotel();
+
+	boolean isEmpty(String rno);
+
+	boolean isDup(String name);
+
+	int addGuest(Guest guest);
+
+	int checkIn(String rno);
+
+	List<Room> roomList();
+
+	int checkOut(String rno);
+
+	int removeGuest(String rno);
+
+	Room roomDetail(String rno);
+
+	List<Guest> guestInfo();
+
+	Guest guestOfRoom(String rno);
+
+	
+}
